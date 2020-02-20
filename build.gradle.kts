fd63fbc9cf19302adfdf9819910bf1001f148332
Eugene Petrenko <eugene.petrenko@gmail.com>
2020-02-20 21:58:49 +0100

    project files

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,3 +54,40 @@ val distUnpacked by tasks.creating(Sync::class.java) {
   includeEmptyDirs = false
   eachFile { path = path.split("/", limit = 2)[1] }
 }
+
+fun JavaExec.copyFromApplicationRun(vararg extraArgs: String) {
+  val runTask = tasks.getByName<JavaExec>("run")
+  dependsOn(tasks.classes)
+  group = runTask.group
+  enableAssertions = true
+
+  doFirst {
+    classpath = runTask.classpath
+    main = runTask.main
+
+    @Suppress("UNNECESSARY_SAFE_CALL", "USELESS_ELVIS")
+    args = (runTask.args?.toList() ?: listOf()) + (args?.toList() ?: listOf()) + extraArgs
+  }
+}
+
+val updateSources by tasks.creating(Exec::class.java) {
+  workingDir(rootProject.rootDir)
+  commandLine("git", "pull")
+}
+
+tasks.classes.configure {
+  dependsOn(updateSources)
+}
+
+val `rr-run` by tasks.creating(JavaExec::class) {
+  dependsOn(updateSources)
+  copyFromApplicationRun("run")
+  group = "teamcity-rr"
+}
+
+val `rr-show` by tasks.creating(JavaExec::class) {
+  dependsOn(updateSources)
+  copyFromApplicationRun("show")
+  group = "teamcity-rr"
+}
+
