1542cd1d5a957f8625cf17a5b90f87595aceaeca
Eugene Petrenko <me@jonnyzzz.com>
2021-01-28 21:07:39 +0100

    tune for windows terminal

--- r-view/src/main/kotlin/com/jonnyzzz/teamcity/rr/ansi.kt
+++ r-view/src/main/kotlin/com/jonnyzzz/teamcity/rr/ansi.kt
@@ -2,8 +2,15 @@ package com.jonnyzzz.teamcity.rr
 
 import com.github.ajalt.mordant.TermColors
 
+private val isWindowsTerminal = System.getenv("WT_SESSION") != null
 
-private val termColors by lazy { TermColors() }
+private val termColors by lazy {
+    when {
+//        isWindowsTerminal -> TermColors(TermColors.Level.ANSI16)
+        //TODO: support IntelliJ console
+        else -> TermColors()
+    }
+}
 fun printProgress(text: String) = println("\n" + termColors.bold(text))
 fun printFinalMessage(text: String) = println("\n" + termColors.bold(text))
 
@@ -14,6 +21,8 @@ fun printWithHighlighting(build: TermColors.() -> String) {
 val supportsLinks by lazy {
     // https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda
     if (System.getenv("TERM_PROGRAM") == "iTerm.app") return@lazy true
+    // windows terminal, https://stackoverflow.com/a/59734130
+    if (isWindowsTerminal) return@lazy true
     //TODO: support Linux and other terminals here
     false
 }
